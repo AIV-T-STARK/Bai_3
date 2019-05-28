9f04bb40289fe4b080a69b174c287754f3e6e7f8
NguyenGiaHao <giahao9899@gmail.com>
2019-05-28 16:59:37 +0700

    basic page -> responsive page

--- js/index.js
+++ js/index.js
@@ -72,4 +72,17 @@ window.onload = function () {
     }
   }
  
+
+
+  let btnCategoryMenu = document.querySelector('.bottom-header-navbar .title');
+  btnCategoryMenu.addEventListener('click', function() {
+      let categoryMenu = document.querySelector('#top-header-wrapper .col-lg-left');
+      categoryMenu.classList.toggle('show');
+  });
+
+  let btnMenu = document.querySelector('.bottom-header-navbar .click-menu');
+  btnMenu.addEventListener('click', function() {
+    let menu = document.querySelector('.bottom-header-navbar ul');
+    menu.classList.toggle('show');
+  })
 }
